cf7dadc874154efa77975757c39fadbd639a2f0a
NeoGlanding <neoglanding@protonmail.com>
2022-06-05 21:18:39 +0700

    untill global validation

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Header, Post, Req } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthDTO } from "./dto";
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('signup')
+    signup(@Body() dto: AuthDTO) {
+        console.log(dto)
+        return this.authService.login();
+    }
+
+    @Post('signin') 
+    signin() {
+        return this.authService.register();
+    }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+@Injectable({})
+export class AuthService {
+    constructor(private prisma: PrismaService) {}
+    login() {
+        
+        return {
+            message: "I have signup"
+        }
+    }
+
+    register() {
+        return {
+            message: "I have signin"
+        }
+    }
+}
